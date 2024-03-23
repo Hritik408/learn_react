56d433b0bf733e2f4622487ec48b1cb271ada141
Hritik408 <chaudharyhritik91@gmail.com>
2024-03-23 23:20:48 +0530

    ..

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Learn_React
+  
+if we delete node modules then we can recreate the nodemodules by using npm install
+how to ignite the app now -> npx parcel index.html(source file), npx means executing a package.
+cdn links are not a good way to get react or reactdom in our project instead use npm -> npm install react
+for reactDOM -> npm install or i react-dom
--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+import React from "react"
+import ReactDOM  from "react-dom/client";
+
+
 const heading = React.createElement("h1", {id: "heading"}, "hello hritik i'm here")
 
 const parent = React.createElement("div", {id: "parent"},[ 
     React.createElement("div", {id: "child"},[
     React.createElement("h1", {}, "hey, i'm h1 tag"),
-    React.createElement("h2", {}, "hey, i'm h2 tag"),
+    React.createElement("h2", {}, "hey, i'm h5 tag"),
     ]), 
      React.createElement("div", {id: "child2"},[
      React.createElement("h1", {}, "hey, i'm h3 tag"),
